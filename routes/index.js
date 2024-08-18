d35902d38295a7f9f47a3fa54b30fd550fe220ac
udonna-cell <orabuezestanley@gmail.com>
2024-08-18 13:39:19 +0100

    basic structure and function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', logo: "it's fun time!"});
+  let data = {
+    title: "Tonikawa over the moon",
+    genre: "romance.drama.slice of life",
+    color: "background: linear-gradient(87deg, #2596be 50%, rgba(37, 150, 190,0.05))"
+  }
+  res.render('index', { title: 'Express', logo: "it's fun time!", isNotify: true, data: [data]});
 });
 
 module.exports = router;
